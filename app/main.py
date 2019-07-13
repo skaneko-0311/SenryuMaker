717a98e636c1a88ea874b3e90ecf96adc30929b4
skaneko-0311 <skaneko.0311@gmail.com>
2019-07-13 10:11:05 -0700

    Connect to database container from other container

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
+import mysql.connector
+ 
+conn = mysql.connector.connect(
+    host="database",
+    port="3306",
+    user="sai",
+    password="sai",
+    database="database"
+)
+conn.ping(reconnect=True)
+print(conn.is_connected())
 print("Hello, Python Docker!")
