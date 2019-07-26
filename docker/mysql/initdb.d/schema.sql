770255ae1e2b202ce90768ae8213453aa4641605
skaneko-0311 <skaneko.0311@gmail.com>
2019-07-26 08:38:03 -0700

    Prepare init tables

--- docker/mysql/initdb.d/schema.sql
+++ docker/mysql/initdb.d/schema.sql
@@ -6,3 +6,38 @@ CREATE TABLE users (
     updated timestamp default current_timestamp on update current_timestamp,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE kamigos (
+    id INT NOT NULL AUTO_INCREMENT,
+    phrase VARCHAR(32) NOT NULL,
+    pronounciation VARCHAR(32) NOT NULL,
+    pillow_phrase VARCHAR(32),
+    user_id INT NOT NULL,
+    created timestamp default current_timestamp,
+    updated timestamp default current_timestamp on update current_timestamp,
+    PRIMARY KEY (id),
+    UNIQUE unique_idx (phrase, pronounciation, pillow_phrase)
+);
+
+CREATE TABLE nakashichis (
+    id INT NOT NULL AUTO_INCREMENT,
+    phrase VARCHAR(32) NOT NULL,
+    pronounciation VARCHAR(32) NOT NULL,
+    pillow_phrase VARCHAR(32),
+    user_id INT NOT NULL,
+    created timestamp default current_timestamp,
+    updated timestamp default current_timestamp on update current_timestamp,
+    PRIMARY KEY (id),
+    UNIQUE unique_idx (phrase, pronounciation, pillow_phrase)
+);
+
+CREATE TABLE shimogos (
+    id INT NOT NULL AUTO_INCREMENT,
+    phrase VARCHAR(32) NOT NULL,
+    pronounciation VARCHAR(32) NOT NULL,
+    user_id INT NOT NULL,
+    created timestamp default current_timestamp,
+    updated timestamp default current_timestamp on update current_timestamp,
+    PRIMARY KEY (id),
+    UNIQUE unique_idx (phrase, pronounciation)
+);
