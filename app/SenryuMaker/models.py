45538a8e887ecb81aba3cec3008cf68e3885d681
skaneko-0311 <skaneko.0311@gmail.com>
2019-07-28 06:28:16 -0700

    Create model classes

--- app/SenryuMaker/models.py
+++ app/SenryuMaker/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id = models.IntegerField()
+    name = models.CharField(max_length=32)
+    email = models.CharField(max_length=32)
+    created = models.DateTimeField()
+    updated = models.DateTimeField()
+
+
+class Kamigo(models.Model):
+    id = models.IntegerField()
+    phrase = models.CharField(max_length=32)
+    pronounciation = models.CharField(max_length=32)
+    pillow_phrase = models.CharField(max_length=32)
+    user_id = models.IntegerField()
+    created = models.DateTimeField()
+    updated = models.DateTimeField()
+
+class Nakashichi(models.Model):
+    id = models.IntegerField()
+    phrase = models.CharField(max_length=32)
+    pronounciation = models.CharField(max_length=32)
+    pillow_phrase = models.CharField(max_length=32)
+    user_id = models.IntegerField()
+    created = models.DateTimeField()
+    updated = models.DateTimeField()
+
+class Shimogo(models.Model):
+    id = models.IntegerField()
+    phrase = models.CharField(max_length=32)
+    pronounciation = models.CharField(max_length=32)
+    user_id = models.IntegerField()
+    created = models.DateTimeField()
+    updated = models.DateTimeField()
